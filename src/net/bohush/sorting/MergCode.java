855f256c36d630aad8c2fd5ce0ce262ba1ad60fd
Ryan Peters <petersryan84@gmail.com>
2017-02-15 15:01:14 -0800

    Create MergCode.java
    just to isolate the relevant merging code

---
+++ src/net/bohush/sorting/MergCode.java
@@ -0,0 +1,69 @@
+public class MergCode{
+  private static final long serialVersionUID = 1L;
+    private int redColumn = -1;
+    private int blueColumn = -1;
+    private int greenColumnStart = -1;
+    private int greenColumnFinish = -1;
+  public void mergeSort(int start, int fin) throws InterruptedException {
+      if ((fin - start) > 0) {
+        mergeSort(start, start + (fin - start) / 2);
+        mergeSort(start + (fin - start) / 2 + 1, fin);
+        merge(start, start + (fin - start) / 2, start + (fin - start) / 2 + 1, fin);
+      }
+    }
+
+    public void merge(int start1, int fin1, int start2, int fin2) throws InterruptedException {
+      int[] list1 = new int[fin1 - start1 + 1];
+      int[] list2 = new int[fin2 - start2 + 1];
+      int[] tmp = new int[list1.length + list2.length];
+      System.arraycopy(list, start1, list1, 0, list1.length);
+      System.arraycopy(list, start2, list2, 0, list2.length);
+      Thread.sleep(2 * sleepTime);
+      repaint();
+        int current1 = 0;
+        redColumn = start1 + current1;
+        int current2 = 0;
+        blueColumn = start2 + current2;
+        int current3 = 0;
+
+      while (current1 < list1.length && current2 < list2.length) {
+        Thread.sleep(2 * sleepTime);
+        repaint();
+        if (list1[current1] < list2[current2]) {
+          tmp[current3++] = list1[current1++];
+          redColumn = start1 + current1;
+        } else {
+          tmp[current3++] = list2[current2++];
+          blueColumn = start2 + current2 - 1;
+        }
+        Thread.sleep(2 * sleepTime);
+        repaint();
+      }
+
+      while (current1 < list1.length) {
+        tmp[current3++] = list1[current1++];
+        redColumn = start1 + current1;
+        Thread.sleep(2 * sleepTime);
+        repaint();
+      }
+
+      while (current2 < list2.length) {
+        tmp[current3++] = list2[current2++];
+        blueColumn = start2 + current2 - 1;
+        Thread.sleep(2 * sleepTime);
+        repaint();
+      }
+
+      redColumn = -1;
+      blueColumn = -1;
+      greenColumnStart = start1;
+      for (int i = 0; i < tmp.length; i++) {
+        greenColumnFinish = start1 + i;
+        list[start1 + i] =  tmp[i];
+        Thread.sleep(2 * sleepTime);
+        repaint();
+      }
+      greenColumnStart = -1;
+      greenColumnFinish = -1;
+	}
+    
